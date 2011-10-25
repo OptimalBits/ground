a9040a3a10dd6c7fc15be0d3952377b09017ff9b
Manuel Astudillo <manuel@optimalbits.com>
2011-10-25 22:14:00 +0200

    Added utility functions, storage, model and collection classes to ginger.

--- ginger.js
+++ ginger.js
@@ -2,10 +2,11 @@
    Ginger MVC framework v0.1
 
    Features:
-   - Modular design based on CommonJS AMD modules.
+   - Modular design.
    - Builds on top of proven libraries such as jQuery, underscore.
    - Clean class hierarchies, based on javascript prototypal inheritance.
    - Property bindings.
+   - Models with persistence and synchronization.
    - Global and Local Events.
    - Undo/Redo Manager.
    - Set of views for common web "widgets".
@@ -29,7 +30,7 @@
    - http://blog.willcannings.com/2009/03/19/key-value-coding-with-javascript/
  */
 
-define(function(){
+define(['math/uuid'], function(uuid){
 
 /**
   Define some useful jQuery plugins.
@@ -75,6 +76,65 @@ if (!Object.create) {
 var ginger = {}
 
 //
+// Utils
+//
+ginger.noop = function(){}
+
+// Filters the keys of an object.
+ginger.filter = function(object, fn){
+  var filtered = {}
+  for(var key in object){
+    var value = object[key]
+    var result = fn(key, value)
+    filtered[key] = result ? result : value
+  }
+  return filtered
+}
+
+ginger.indexOf = function(array, val, iter, isSorted){
+  if(iter){
+    //if(isSorted){
+      for(var i=0, len=array.length;i<len; i++){
+        if(val===iter(array[i])){
+          return i
+        }
+      }
+    //}
+  }else{
+    return _.indexOf(collection, key)
+  }
+}
+
+// Apply asynchronous functions to every element in the array
+ginger.asyncForEach = function(array, fn, callback) {
+  var deferred = $.Deferred()
+  var completed = 0;
+  if(array.length === 0) {
+    if(_.isUndefined(callback)===false){
+      callback(); // done immediately
+    }
+    deferred.resolve()
+  }
+  var len = array.length;
+  for(var i = 0; i < len; i++) {
+    fn(array[i], function(err) {
+      if(_.isNull(err)||_.isUndefined(err)){
+        completed++;
+        if(completed === array.length) {
+          if(_.isUndefined(callback)===false){
+            callback();
+          }
+          deferred.resolve()
+        }
+      }else{
+        deferred.reject()
+      }
+    });
+  }
+  return deferred
+};
+
+//
 // Event Emitter
 // (based on original work by Oliver Caldwell, olivercaldwell.co.uk)
 // Dual licensed under the MIT or GPL Version 2 licenses.
@@ -97,30 +157,23 @@ EventEmitter.prototype._getListeners = function(){
   * @param {Function} listener Function to be executed when the specified event is emitted
   * @returns {Object} The current instance of EventEmitter to allow chaining
   */
-EventEmitter.prototype.addListener = function(eventName, listener) {
+EventEmitter.prototype.on = function(eventNames, listener) {
+  var events = eventNames.split(' ')
   var listeners = this._getListeners()
-  if(listeners[eventName]) {
-    listeners[eventName].push(listener);
+  
+  for(var i=0, len=events.length;i<len;i++){
+    var event = events[i]
+    if(listeners[event]) {
+      listeners[event].push(listener);
+    }
+    else{
+      listeners[event] = [listener];
+    }
+    this.emit('newListener', event, listener);
   }
-  else {
-    listeners[eventName] = [listener];
-  }
-		
-  // Emit the new listener event
-  this.emit('newListener', eventName, listener);
-
   return this;
 }
 
-/**
-  * Assigns a listener to the specified event (alias for addListener)
-  * 
-  * @param {String} eventName Name of the event to assign the listener to
-  * @param {Function} listener Function to be executed when the specified event is emitted
-  * @returns {Object} The current instance of EventEmitter to allow chaining
-  */
-EventEmitter.prototype.on = EventEmitter.prototype.addListener;
-	
 /**
   * Emits the specified event running all listeners associated with it
   * 
@@ -130,18 +183,23 @@ EventEmitter.prototype.on = EventEmitter.prototype.addListener;
   */
 EventEmitter.prototype.emit = function(eventName) {
   var listeners = this._getListeners()
-  
-  if(listeners[eventName]) {
-    var args = Array.prototype.slice.call(arguments, 1);
-    var eventListeners = listeners[eventName]
-    
-    // Loop over the listeners executing them
-    for(var i = 0; i < eventListeners.length; i += 1) {
-      eventListeners[i].apply(null, args);
-    }
+  var eventListeners = []
+  if(listeners[eventName]){
+    eventListeners = listeners[eventName]
+  }
+  if(listeners['*']){
+    this._fire(eventListeners, arguments)
+  }
+  if(listeners[eventName]){
+    var args = _.rest(arguments)
+    this._fire(eventListeners, args)
   }		
-	
   return this
+}
+EventEmitter.prototype._fire = function(eventListeners, args){
+  for(var i=0, len=eventListeners.length; i < len; i ++) {
+    eventListeners[i].apply(null, args);
+  }
 }
 	
 /**
@@ -153,12 +211,12 @@ EventEmitter.prototype.emit = function(eventName) {
 EventEmitter.prototype.listeners = function(eventName) {
   var listeners = this._getListeners()
   if(listeners[eventName]) {
-			return listeners[eventName];
+    return listeners[eventName];
   }
   else {
-			listeners[eventName] = [];
-			return listeners[eventName];
-		}
+    listeners[eventName] = [];
+    return listeners[eventName];
+  }
 }
 
 /**
@@ -171,16 +229,11 @@ EventEmitter.prototype.listeners = function(eventName) {
 EventEmitter.prototype.once = function(eventName, listener) {
   var ee = this
   
-  // Create a wrapper function
   function wrapper() {
-			// Call the listener and pass down the arguments
 			listener.apply(null, arguments);
-			
-			// Remove the listener
-			ee.removeListener(eventName, wrapper);
+      ee.removeListener(eventName, wrapper);
 		}
 		
-		// Add the listener for the wrapper
 		return ee.addListener(eventName, wrapper);
 };
 	
@@ -191,21 +244,23 @@ EventEmitter.prototype.once = function(eventName, listener) {
   * @param {Function} listener Listener function to be removed
   * @returns {Object} The current instance of EventEmitter to allow chaining
   */
-EventEmitter.prototype.removeListener = function(eventName, listener) {
+EventEmitter.prototype.off = function(eventNames, listener) {
+  var events = eventNames.split(' ')
   var listeners = this._getListeners()
   
-  if(listeners[eventName]) {
-    var index = _.indexOf(listeners[eventName], listener);
-			
-			if(index !== -1) {
-				listeners[eventName].splice(index, 1);
+  for(var i=0, len=events.length;i<len;i++){
+    var event = events[i]
+    if(listeners[event]) { 
+      var index = _.indexOf(listeners[event], listener);
+      if(index !== -1) {
+        listeners[event].splice(index, 1);
 			}
 		}
 		else {
-      listeners[eventName] = [];
+      listeners[event] = [];
 		}
-		
-		return this;
+  }
+  return this;
 };
 	
 /**
@@ -214,16 +269,21 @@ EventEmitter.prototype.removeListener = function(eventName, listener) {
   * @param {String} eventName Name of the event to remove the listeners from
   * @returns {Object} The current instance of EventEmitter to allow chaining
   */
-EventEmitter.prototype.removeAllListeners = function(eventName) {
+EventEmitter.prototype.removeAllListeners = function(eventNames) {
+  var events = eventNames.split(' ')
   var listeners = this._getListeners()
-  listeners[eventName] = [];
+  for(var i=0, len=events.length;i<len;i++){
+    listeners[events[i]] = [];
+  }
   return this;
 }
 
-EventEmitter.prototype.addObserver = function(eventName, listener){
-// DEPRECATED: just for backwards compatibility
-  this.on(eventName, listener)
-}
+/**
+  Aliases
+*/
+EventEmitter.prototype.addListener = EventEmitter.prototype.on;
+EventEmitter.prototype.addObserver = EventEmitter.prototype.on
+EventEmitter.prototype.removeListener = EventEmitter.prototype.off
 
 //
 // Undo Manager
@@ -263,12 +323,12 @@ UndoManager.prototype.beginGroup = function(){
 UndoManager.prototype.endGroup = function(){
   ;(function(group){
     this.action( function(){
-      for(var i=0; i<group.length;i++){
+      for(var i=0, len = group.length; i<len; i++){
         group[i].do(group[i].fn)
       }
     },
     function(){
-      for(var i=0; i<group.length;i++){
+      for(var i=0, len=group.length; i<len;i++){
         group[i].undo(group[i].fn)
       }
     })
@@ -326,8 +386,22 @@ function Inherit(Sub, Super){
   }
 } 
 
-ginger.Declare = function( Super, Sub ){
-  Inherit(Sub, Super)  
+ginger.Declare = function(Super, Sub, staticOrName, name){
+  Inherit(Sub, Super)
+  if(Super.__staticMethods){
+    _.extend(Sub, Super.__staticMethods)
+  }
+  if(staticOrName){
+    if(_.isObject(staticOrName)){
+      Sub.__staticMethods = staticOrName
+      _.extend(Sub, staticOrName)
+    }else{
+      name = staticOrName
+    }
+  }
+  if(name){
+    Sub.__name = Sub.prototype.__name = name
+  }
   return Sub
 }
 
@@ -335,7 +409,7 @@ ginger.Declare = function( Super, Sub ){
 //  Base Class - All Ginger classes derive from this one.
 //
 ginger.Base = function(){
-  this.bindings = {}
+  this._bindings = {}
 }
 
 _.extend(ginger.Base.prototype, EventEmitter.prototype)
@@ -381,7 +455,7 @@ ginger.Base.prototype.bind = function(key, object, objectKey){
   var srcListener = _.bind(this.set, this, key)
   object.on(dstKey, srcListener)
   
-  this.bindings[key] = [dstListener, object, dstKey, srcListener];
+  this._bindings[key] = [dstListener, object, dstKey, srcListener];
   
   // sync
   this.set(key, object[dstKey])
@@ -394,7 +468,7 @@ ginger.Base.prototype.bind = function(key, object, objectKey){
 
 */
 ginger.Base.prototype.unbind = function(key){
-  var bindings = this.bindings
+  var bindings = this._bindings
   if( (bindings!=null) && (bindings[key]) ){
     var binding = bindings[key]
     this.removeListener(key, binding[0])
@@ -441,14 +515,312 @@ ginger.Base.prototype.undoSet = function(key, value, fn){
   this.set(key, value)
   this.endUndoSet(key, fn)
 }
+// -----------------------------------------------------------------------------------
+//
+// Storage
+// (requires localStorage)
+// -----------------------------------------------------------------------------------
+var Storage = {}
+Storage.findById = function(bucket, id){
+  var objectId = bucket+'@'+id
+  for (var i=0, len=localStorage.length;i<len;i++){
+    var key = localStorage.key(i)
+    if(key === objectId){
+      return JSON.parse(localStorage[key])
+    }
+  }
+  return null
+}
+Storage.all = function(bucket){
+  var collection = []
+  for (var i=0, len=localStorage.length;i<len;i++){
+    var key = localStorage.key(i)
+    if(key.split('@')[0] === bucket){
+      collection.push(JSON.parse(localStorage[key]))
+    }
+  }
+  return collection
+}
+Storage.update = function(bucket, id, args){
+  var obj = Storage.findById(bucket, id)
+  if(obj){
+    _.extend(obj, args)
+    Storage.save(bucket, id, obj)
+  }else{
+    Storage.save(bucket, id, args)
+  }
+}
+Storage.save = function(bucket, id, args){
+  var objectId = bucket+'@'+id
+  localStorage[objectId] = JSON.stringify(args)
+}
+Storage.remove = function(bucket, id){
+  var objectId = bucket+'@'+id
+  localStorage.removeItem(objectId)
+}
+ginger.Storage = Storage
+
+/*
+  Storage.observe(bucket, key, function(obj){
+  //
+  })
+  Storage.save(bucket, key, obj)
+  Storage.remove(bucket, key)
+  Storage.update(bucket, key, obj) // Lazy update.
+  
+  How to handle locks when offline? (ask the user what to do in case of conflict!)
+*/
 
 // -----------------------------------------------------------------------------------
+//
+// Models
+//
+// -----------------------------------------------------------------------------------
+ginger.Model = ginger.Declare(ginger.Base, function(args){
+  this.super(ginger.Model)
+  _.extend(this, args)
+  _.defaults(this, {
+    socket:ginger.Model.socket,
+    __model:true
+  })
+  if(_.isUndefined(this._id) || _.isNull(this._id)){
+    this.cid = uuid()
+  }else{
+    this.cid = this._id
+  }
+},
+{ // TODO: use socket.connected to avoid unnecessary emits.
+  findById : function(id, fn){
+    var model = this,
+        socket = ginger.Model.socket,
+        name = this.__name,
+        bucket = name+'s'
+    if(socket){
+      socket.emit('read:'+name, id, function(args){
+        args = args !== null ? args : Storage.findById(bucket, id)
+        _instantiate(model, args, fn)
+      })
+    }else{
+      var args = Storage.findById(bucket, id)
+      _instantiate(this, args, fn)
+    }
+    return this
+  },
+  all : function(submodel, fn){
+    var model = this,
+        socket = ginger.Model.socket,
+        name = this.__name,
+        urn = 'read:'+this.__name,
+        bucket = this.__name+'s',
+        id = null
+    if(_.isFunction(submodel)){
+      fn = submodel
+      submodel = model
+    }else{
+      id = this.cid
+      urn += ':'+submodel.__name
+      bucket += '@'+id+'@'+submodel.__name
+    }
+    if(socket){
+      socket.emit(urn, id, function(array){
+        array = array !== null ? array : Storage.all(bucket)
+        _instantiateCollection(submodel, array, function(collection){
+          socket.on('add:'+bucket,function(entry){
+            collection.add(entry)
+          }).on('update:'+bucket, function(entry){
+            collection.update(entry)
+          }).on('remove:'+bucket, function(entryId){
+            collection.remove(entryId)
+          })
+          fn(collection)
+        })
+      })
+    }else{
+      var collection = Storage.all(bucket)
+      _instantiateCollection(submodel, collection, fn)
+    }
+    return this
+  },
+  first : function(fn){
+    this.all(function(collection){
+      fn(collection?collection.first() : null)
+    })
+  },
+})
+
+var _instantiate = function(model, args, fn){
+  var instance = args !== null ? new model(args) : null
+  if(instance){
+    instance.init(function(){
+      fn(instance)
+    })
+  }else{
+    fn(null)
+  }
+}
+var _instantiateCollection = function(model, array, callback){
+  if((array)&&(array.length>0)){
+    collection = new ginger.Collection()
+    ginger.asyncForEach(array, function(data, fn){
+      _instantiate(model, data, function(instance){
+        if(instance){
+          collection.add(instance)
+          fn()
+        }else{
+          fn('Error instantiating instance:'+data)
+        }
+      })
+    }, function(err){
+      if(err){
+        callback(null)
+      }else{
+        callback(collection)
+      }
+    })
+  }else{
+    callback(null)
+  }
+}
+
+ginger.Model.prototype.init = function(fn){
+  fn(this)
+}
+ginger.Model.prototype.all = function(model, fn){
+  if(this._id){
+    model.all(this.__name+'s/'+this._id+'/', fn)
+  }else{
+    fn(null)
+  }
+}
+ginger.Model.prototype.save = function(socket, fn){
+  var args = this.toArgs()
+  this.update(socket, args, fn)
+}
+ginger.Model.prototype.update = function(socket, args, fn){
+  var model = this,
+      name = this.__name,
+      bucket = name+'s'
+  if(socket){
+    if(this._id) {//[this._id, args]
+      socket.emit('update:'+name, {id:this._id, attrs:args}, function(){
+        ginger.Storage.update(bucket, model._id, args)
+        fn?fn(model._id):ginger.noop()
+      })
+    }else{
+      socket.emit('create:'+name, attrs, function(id){
+        args.cid = model._id = id
+        ginger.Storage.remove(bucket, model.cid)
+        ginger.Storage.update(bucket, id, args)
+        fn?fn(id):ginger.noop()
+      })
+    }
+  }else{
+    ginger.Storage.save(bucket, args.cid, args)
+    fn?fn(this.cid):ginger.noop()
+  }
+}
+
+ginger.Model.prototype.toArgs = function(){
+  var args = {}
+  
+  for(var key in this){
+    if((_.isFunction(this[key])===false)&&(key[0] !== '_')){
+      args[key] = this[key]
+      //TODO: Add support for nested models.
+    }
+  }
+  return args
+}
+ginger.Model.socket = null
+
+/**
+  A collection is a set of ordered models. 
+  It provides delegation and proxing of events.
+**/
+ginger.Collection = ginger.Declare(ginger.Base, function(){
+  this.super(ginger.Collection)
+  this._models = []
+})
+ginger.Collection.prototype.add = function(model, sortByFn){
+  if(_.isArray(model)){
+    var models = sortByFn ? _.sortBy(model, sortByFn) : model
+    for(var i=0, len=model.length;i<len;i++){
+      this._add(model[i])
+    }
+  }else{
+    this._add(model, sortByFn)
+  }
+}
+ginger.Collection.prototype.remove = function(model){
+  var i = _.indexOf(this._models, model)
+  if(i>=0){
+    model.off('*', this._proxyEvent)
+    this._models.splice(i,1)
+  }
+}
+ginger.Collection.prototype.update = function(model, sortByFn){
+  var i = ginger.indexOf(this._models, model._id, function(a){return a._id}, true)
+  if(i>=0){
+    var updated = _.extend(this._models[i], model)
+    if(sortByFn){
+      this._models.splice(i,1)
+      i = _.sortedIndex(this._models, updated, sortByFn)
+      this._models.splice(i, 0, updated)
+    }
+  }
+}
+ginger.Collection.prototype.first = function(){
+  if((this._models)&&(this._models.length>0)){
+    return this._models[0]
+  }else{
+    return null
+  }
+}
+ginger.Collection.prototype._add = function(model, sortByFn){
+  var _this = this
+  if(sortByFn){
+    var i = _.sortedIndex(this._models, model, sortByFn)
+    this._models.splice(i, 0, model)
+  }else{
+    this._models.push(model)
+  }
+  model.on('*', this._proxyEvent)
+}
+ginger.Collection.prototype._proxyEvent = function(event){
+  this.emit(event, _.rest(arguments))
+}
+
+// Underscore methods that we want to implement on the Collection.
+var methods = ['forEach', 'each', 'map', 'reduce', 'reduceRight', 'find', 'detect',
+               'filter', 'select', 'reject', 'every', 'all', 'some', 'any', 'include',
+               'contains', 'invoke', 'max', 'min', 'sortBy', 'sortedIndex', 'toArray', 'size',
+               'first', 'rest', 'last', 'without', 'indexOf', 'lastIndexOf', 'isEmpty', 'groupBy']
+
+// Mix in each Underscore method as a proxy to `Collection#_models`.
+_.each(methods, function(method) {
+  ginger.Collection.prototype[method] = function() {
+    return _[method].apply(_, [this._models].concat(_.toArray(arguments)))
+  }
+})
+// -----------------------------------------------------------------------------------
+
+// FIX:
+// $el.remove will indeed destroy all the DOM nodes in $el including $el
+// this means that if we want to be able to re-render a removed node, we need
+// to be able to re create the whole sub DOM tree from this view.
+// therefore $el must be created in render. But since $el could be something
+// different than a div, we will introcude the tag property, which will be used
+// in render to create the proper element.
 ginger.View = ginger.Declare(ginger.Base, function(classNames){
   this.super(ginger.View)
   this.$el = $('<div>', {class:classNames})
+  
+  this.classNames = classNames
+  this.tag = '<div>'
 })
 
 ginger.View.prototype.render = function($parent){
+// this.$el = $(tag, {class:this.classNames})
   this.$parent = $parent
   return this.$el.appendTo($parent)
 }
@@ -521,13 +893,6 @@ ginger.Controller = ginger.Declare(ginger.Base, function(view){
   this.super(ginger.Controller)
   this.view = view
 })
-// -----------------------------------------------------------------------------------
-ginger.Model = ginger.Declare(ginger.Base, function(){
-  this.super(ginger.Model)
-  // TODO: Add methods related to synchronizing model with server, add support 
-  // for caching, client side persistence, etc.
-})
-
 // -----------------------------------------------------------------------------------
 //
 // Views
