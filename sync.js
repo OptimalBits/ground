e954b8b993c99c02063cc9b29ee58396cc7aeef1
Manuel Astudillo <manuel@optimalbits.com>
2012-03-16 11:22:34 +0100

    Several updated to the sync server module.

--- sync.js
+++ sync.js
@@ -31,53 +31,63 @@
 */
 
 var sync = function(subClient, pubClient, sio, sockets){
+  var self = this;
+    
   this.pubClient = pubClient;
-  
-  if(!sockets){
-    sockets = sio.sockets;
-  }
-  
-  sio.on('connection', function(socket){
-    console.log("Connected in the Sync Module");
-    socket.on('sync', function(id){
-      if(check){
-        if (check(socket.id, id)){
-          socket.join(id);
-        }
-      }else{
-        console.log("Socket started synchronization for id:"+id);
-        socket.join(id);
-      }
-    });
     
-    socket.on('unsync', function(id){
-      socket.leave(id);
-    });
-  });
-
-  
-  subClient.subscribe('delete:');
-
-  subClient.psubscribe('add:*')
-  subClient.psubscribe('remove:*');
-  subClient.psubscribe('update:*');
-
-  subClient.on('message', function(channel, msg){
-    sockets.in(msg._id).emit(channel+msg._id, msg);
-  });
-
-  subClient.on('pmessage', function(pattern, channel, msg){
-    var comps = channel.split(':'), 
-        cmd = comps[0],
-        objId = comps[1],
-        bucket = comps[2];
-    
-    if(pattern === 'update:*'){
-      sockets.in(msg._id).emit(cmd+':'+objId, JSON.parse(msg));
-    }else{
-      sockets.in(objId).emit(cmd+':'+bucket, msg);
+  if(sio){
+    if(!sockets){
+      sockets = sio.sockets;
     }
-  });
+    
+    sockets.on('connection', function(socket){
+      console.log("Socket %s connected in the Sync Module", socket.id);
+      socket.on('sync', function(id){
+        if(self.check){
+          if (check(socket.id, id)){
+            socket.join(id);
+          }
+        }else{
+          console.log("Socket %s started synchronization for id:%s",socket.id, id);
+          socket.join(String(id));
+        }
+      });
+    
+      socket.on('unsync', function(id){
+        console.log("Socket %s stopped synchronization for id:%s", socket.id, id);
+        socket.leave(id);
+      });
+    });
+
+    subClient.subscribe('delete:');
+
+    subClient.on('message', function(channel, msg){
+      sockets.in(msg._id).emit(channel+msg._id, msg);
+    });
+  
+    subClient.psubscribe('add:*')
+    subClient.psubscribe('remove:*');
+    subClient.psubscribe('update:*');
+
+    subClient.on('pmessage', function(pattern, channel, msg){
+      var comps = channel.split(':'), 
+          cmd = comps[0],
+          objId = comps[1],
+          bucket = comps[2];
+          
+      msg = JSON.parse(msg);
+    
+      if(pattern === 'update:*'){
+        sockets.in(objId).json.emit(cmd+':'+objId, msg);
+      }else{
+        sockets.in(objId).emit(cmd+':'+objId+':'+bucket, msg);
+      }
+      // TODO: Implement except to avoid the sender to receive the message it sent
+      // Note that this has the implication that if you have several instances of one
+      // model or connection in the same browser, they will not be notificated, so
+      // some kind of intra model notification will be needed (Model Factory?)
+    });
+  }
 }
 
 sync.prototype.update = function(id, doc){
@@ -88,13 +98,16 @@ sync.prototype.delete = function(id){
   this.pubClient.publish('delete:', id);
 }
 
-sync.prototype.add = function(objId, bucket, id){
-  this.pubClient.publish('add:'+objId+':'+bucket, id);
+sync.prototype.add = function(objId, bucket, ids){
+  this.pubClient.publish('add:'+objId+':'+bucket, JSON.stringify(ids));
 }
 
-sync.prototype.remove = function(objId, bucket, id){
-  this.pubClient.publish('remove:'+objId+':'+bucket, id);
+sync.prototype.remove = function(objId, bucket, ids){
+  this.pubClient.publish('remove:'+objId+':'+bucket, JSON.stringify(ids));
 }
 
 exports = module.exports = sync; 
+
+
+
 
