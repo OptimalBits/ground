195f5abed3b1021896c045c2bf45c01b11c91d65
Manuel Astudillo <manuel@optimalbits.com>
2012-04-13 10:12:48 +0200

    Added table view example.

---
+++ examples/example_server.js
@@ -0,0 +1,9 @@
+var express = require('express'),
+    app = express.createServer(),
+    staticDir = __dirname + '/../';
+
+app.use(express.static(staticDir));
+app.use(express.static(__dirname+'/tables'));
+
+app.listen(8080);
+console.log("Started test server at port: %d in %s mode", app.address().port, app.settings.env);
