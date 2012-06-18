44b00c51a2048b247d0101d31e5653c31df9d540
Sebastian Astudillo <sebastian@optimalbits.com>
2012-06-18 17:35:25 +0200

    added empty function to modals

--- ginger.js
+++ ginger.js
@@ -2547,6 +2547,23 @@ Views.Modal.prototype.enable = function() {
 Views.Modal.prototype.content = function(content){
   this.$content.html(content);
 }
+/* Empty all modal forms*/
+Views.Modal.prototype.empty = function() {
+  this.$el.find(':input').each(function() {
+    switch(this.type) {
+      case 'password':
+      case 'select-multiple':
+      case 'select-one':
+      case 'text':
+      case 'textarea':
+        $(this).val('');
+        break;
+      case 'checkbox':
+      case 'radio':
+        this.checked = false;
+    }
+  });
+}
 //------------------------------------------------------------------------------
 Views.Button = View.extend( function Button(options){
   this.super(Views.Button)
