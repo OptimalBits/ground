123f57ac57d1efc0c56b6c16fdaed41bce8325d4
Aron Kornhall <git@kornhall.se>
2012-11-21 22:33:49 +0100

    Optimized cache.ts

--- lib/cache.ts
+++ lib/cache.ts
@@ -26,14 +26,114 @@ import Base = module('./base');
 // TODO: factorize storage into a generic storage interface.
 var ls = localStorage;
 
+interface IndexElem {
+  next: number;
+  prev: number;
+  key: string;
+}
+
+class Index {
+  private index : IndexElem[];
+  private tail : IndexElem;
+  private first : number;
+  private last : number;
+
+  private unusedKeys : number[];
+
+  constructor(){ 
+    this.tail = {
+      prev: 0,
+      next: 0,
+      key: ''
+    };
+    this.index = [this.tail];
+    this.first = this.last = 0;
+    this.unusedKeys = [];
+  }
+
+  // get an unused index for new elements
+  private newIdx() {
+    if (this.unusedKeys.length > 0) {
+      return this.unusedKeys.pop();
+    } else {
+      return this.index.length;
+    }
+  }
+    
+  // Insert key first in the index
+  addKey(key: string) : number {
+    var elem = {
+      prev : this.last, // circular
+      next : this.first,
+      key : key
+    };
+
+    var idx = this.newIdx();
+    this.index[idx] = elem;
+
+    var firstElem = this.index[this.first];
+    var lastElem = this.index[this.last];
+
+    firstElem.prev = idx;
+    lastElem.next = idx; // circular
+
+    this.first = idx;
+    return idx;
+  }
+
+  // Touch an element and put it first in the index
+  touch(idx: number) : number {
+    var key = this.remove(idx);
+    return this.addKey(key);
+  }
+
+  // Remove element idx from the index
+  remove(idx: number) : string {
+    // don't allow removal of tail
+    if (idx === 0) return null;
+
+    var elem = this.index[idx];
+    var nextElem = this.index[elem.next];
+    var prevElem = this.index[elem.prev];
+
+    // remove elem from list
+    nextElem.prev = elem.prev;
+    prevElem.next = elem.next;
+
+    // handle edge cases
+    if (idx === this.first) {
+      this.first = elem.next;
+    } else if (idx === this.last) {
+      this.last = elem.prev;
+    }
+
+    // mark the index as unused
+    this.unusedKeys.push(idx);
+
+    return elem.key;
+  }
+
+  // Get the last element in the index
+  getLast() : string {
+    if (this.first === this.last) {
+      return null;
+    } else {
+      // return last element (element before tail)
+      return this.index[this.tail.prev].key;
+    }
+  }
+}
+
 export class Cache extends Base.Base {
   private maxSize : number;
   private size : number = 0;
   private map : {};
+  private index : Index;
   private length : number = 0;
   
   constructor(maxSize? : number = 5*1024*1024){ 
     super();
+    this.maxSize = maxSize;
     this.populate();
   }
   /**
@@ -57,7 +157,11 @@ export class Cache extends Base.Base {
   }
   
   setItem(key, value){
-    var time = Date.now(), old = this.map[key], requested = value.length;
+    var time = Date.now();
+    var old = this.map[key];
+    value = String(value);
+    var requested = value.length;
+    var idx;
     
     if(old){
       requested -= old.size;
@@ -68,24 +172,34 @@ export class Cache extends Base.Base {
       ls[this.key(key, time)] = value;
 
       if(old){
-        // Avoid remove the set item
-        if(old.time != time){ 
+        // Avoid removing the set item
+        if(old.time !== time){ 
           this.remove(key, old.time);
         }
+
+        idx = old.idx;
+        this.index.touch(idx);
       }else{
         this.length++;
+        idx = this.index.addKey(key);
       }
-      this.map[key] = {time:time, size:value.length};
+      this.map[key] = {
+        time: time,
+        size: value.length,
+        idx: idx
+      };
     }
   }
   
   removeItem(key){
     var item = this.map[key];
-    if(item){
+    if (item){
       this.remove(key, item.time);
       this.size -= item.size;
       delete this.map[key];
       this.length--;
+
+      this.index.remove(item.idx);
     }
   }
   
@@ -111,22 +225,41 @@ export class Cache extends Base.Base {
   }
   
   private populate(){
+    var that = this;
     var i, len, key, s, k, size;
     this.size = 0;
     this.map = {};
+    this.index = new Index();
     for (i=0, len=ls.length;i<len;i++){
       key = ls.key(i);
       if (key.indexOf('|') != -1){
+        //TODO: length is the strlen here. check if ok
         size = ls[key].length;
+
         s = key.split('|');
         // avoid possible duplicated keys due to previous error
         k = s[0];
-        if(!this.map[k] || this.map[k].time > s[1]){
+        if(!this.map[k] || this.map[k].time < s[1]){
           this.map[k] = {time : s[1], size : size}
         }
         this.size += size;
       }
     }
+
+    // sort keys by timestamp (old->new)
+    var list = _.map(this.map, function(item, key) {
+      return {time:item.time, key:key}
+    });
+    var sorted = _.sortBy(list, function(item){
+      return item.time;
+    });
+    // add keys to index
+    _.each(sorted, function(elem) {
+      var idx = that.index.addKey(elem.key);
+      // TODO: better way in ts?
+      that.map[elem.key].idx = idx;
+    });
+
     this.length = _.size(this.map);
   }
   
@@ -135,17 +268,16 @@ export class Cache extends Base.Base {
   //
   private makeRoom(size){
     var target = this.maxSize - size;
+    var last;
     if(this.size > target){
-      if(target<0){
+      if(target < 0){
         return false;
       }else{
-        // TODO: We need to optimize this.(move to populate and keep sorted in order).
-        var list = _.map(this.map, function(item, key){return {time:item.time, key:key}});
-        var sorted = _.sortBy(list, function(item){return item.time;});
-        var index = sorted.length-1;
-    
-        while ((this.size > target) && (index >= 0)){
-          this.removeItem(sorted[index--].key);
+        last = this.index.getLast();
+        while (this.size > target) {
+          if (last === null) return false;
+          this.removeItem(last);
+          last = this.index.getLast();
         }
       }
     }
