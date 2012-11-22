f88ec624c805b8f335ba469c16f5919611cc7352
Manuel Astudillo <manuel@optimalbits.com>
2012-11-22 21:44:17 +0100

    Removed localStorage clear from socket storage unit tests.

--- test/test_storage_socket.js
+++ test/test_storage_socket.js
@@ -1,8 +1,6 @@
 define(['socket'], function(Storage){
 
-describe('Socket.io Storage', function(){
-  
-  localStorage.clear();
+describe('Socket.io Storage', function(){  
   storage = new Storage.Socket(socket);
   storageType = "Socket Storage"
   curl(['test/test_storage'])
