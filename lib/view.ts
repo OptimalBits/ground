c69f57db8c9a96d7d5feb80ce11a7c9bb0fa1885
Manuel Astudillo <manuel@optimalbits.com>
2012-12-03 21:17:32 +0100

    Initial implementation of base View class.

---
+++ lib/view.ts
@@ -0,0 +1,82 @@
+/**
+  Ground Web Framework (c) 2011-2012 Optimal Bits Sweden AB
+  MIT Licensed.
+*/
+/**
+  View Class
+  
+  This class represents a View in a MVC architecture.
+  
+  Events:
+    
+*/
+/// <reference path="../third/underscore.browser.d.ts" />
+/// <reference path="../third/jquery.d.ts" />
+
+import Base = module('./base');
+
+
+class View extends Base.Base
+{
+  public tag: string;
+  public classNames: string[];
+  public $el: JQuery;
+  public $parent: JQuery;
+  public css: {};
+  
+  constructor(classNames: string[], css?: {} = {}, tag?: string = 'div')
+  {
+    super();
+    this.classNames = classNames;
+    this.css = css;
+    this.tag = tag;
+    
+    this.$el = $(this.tag);
+    this.$el.css(this.css);
+  }
+
+  render($parent)
+  {
+    this.$parent = $parent || this.$parent;
+    this.$parent && this.$el && this.$el.detach().appendTo(this.$parent);
+    return this.$el;
+  }
+  
+  refresh()
+  {
+    this.$parent && this.render(this.$parent);
+  }
+  
+  clean()
+  {
+    this.$el.detach();
+  }
+  
+  remove()
+  {
+    this.$el.remove()
+    this.$el = null;
+  }
+  
+  disable(disable)
+  {
+    console.log(this+" does not implement disable")
+  }
+  
+  hide(duration, easing, callback)
+  {
+    this.$el.hide(arguments)
+  }
+  
+  show(duration, easing, callback) 
+  {
+    this.$el.show(arguments)
+  }
+  
+  destroy()
+  {
+    this.remove();
+    super.destroy();
+  }
+}
+
