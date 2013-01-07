743a5600e306433b2d5bfaaa89230565d89732e3
Manuel Astudillo <manuel@optimalbits.com>
2013-01-07 22:50:39 +0100

    Added a DOM element maker

--- lib/dom.ts
+++ lib/dom.ts
@@ -5,6 +5,9 @@
   DOM Utils.
     
   Simple and lightweight utility functions for cross browser DOM manipulation.
+  
+  Some functions based or inspired by 140medley:
+  https://github.com/honza/140medley
 */
 
 module Gnd {
@@ -18,7 +21,7 @@ module Gnd {
  *
  * http://jsperf.com/simple-jquery-selector-vs-140medley/2
  */
-export function $$(selector: string, context: Element): Element
+export function $$(selector: string, context?: Element): Element
 {
   var el = context || document;
   switch(selector[0]){
@@ -30,6 +33,34 @@ export function $$(selector: string, context: Element): Element
 
 export function isElement(object) {
   return object && object.nodeType === Node.ELEMENT_NODE
+}
+
+/*
+ * Create DOM element
+ *
+ * Usage:
+ *   var el = m('<h1>Hello</h1>');
+ *   document.body.appendChild(el);
+ *
+ *
+ *            DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+ *                    Version 2, December 2004
+ *
+ * Copyright (C) 2011 Jed Schmidt <http://jed.is> - WTFPL
+ * More: https://gist.github.com/966233
+ *
+ */
+export function makeElement(html: string): DocumentFragment
+{
+  var container = document.createElement("p");      // create a container element,
+  var fragment = document.createDocumentFragment(); // create a fragment.
+  container.innerHTML = html;                       // write the HTML to it, and
+  
+  while (container = <HTMLElement> container.firstChild){ // the container element has a first child
+    fragment.appendChild(container);             // append the child to the fragment,
+  } 
+
+  return fragment;
 }
 
 /**
