644590e195484e7a603f00bad41f9ca3c482bc31
Aron Kornhall <git@kornhall.se>
2013-01-11 17:11:19 +0100

    Adapted chat example to work with the ts branch

--- examples/example_server.js
+++ examples/example_server.js
@@ -1,3 +1,7 @@
+require('require-typescript');
+
+var Gnd = require('../gnd-server.ts');
+
 var express = require('express'),
     app = express.createServer(),
     mongoose = require('mongoose'),
@@ -12,32 +16,45 @@ var express = require('express'),
 app.use(express.static(staticDir));
 app.use(express.static(__dirname));
 
-// Setup a mongo DB. This is used by the chat example
+// mongoose Schemas
 var Message = new Schema({
   text : {type: String}
 });
-mongoose.model('Message', Message);
 
 var Chat = new Schema({
   messages :  [{ type: Schema.ObjectId, ref: 'Message' }]
 });
-mongoose.model('Chat', Chat);
 
-var models = {
-  Message: mongoose.model('Message'),
-  messages: mongoose.model('Message'),
-  Chat: mongoose.model('Chat')
+Chat.statics.add = function(id, setName, itemIds, cb){
+  var update = {$addToSet: {}};
+  update.$addToSet[setName] = {$each:itemIds};
+  this.update({id:id}, update, cb);
 };
 
-mongoose.connect('mongodb://localhost/exampleDB', function(){
-  mongoose.connection.db.executeDbCommand( {dropDatabase:1}, function(err, result) { 
-    console.log(err); 
-    console.log(result); 
+var models = {
+  Message: mongoose.model('Message', Message),
+  messages: mongoose.model('Message', Message),
+  Chat: mongoose.model('Chat', Chat)
+};
 
+// Setup mongodb
+mongoose.connect('mongodb://localhost/exDB', function(){
+  mongoose.connection.db.executeDbCommand( {dropDatabase:1}, function(err, result) {
+    console.log(result);
+    mongoose.disconnect(function(){
+      mongoose.connect('mongodb://localhost/exDB');
+  
+      app.listen(8080);
+      console.log("Started test server at port: %d in %s mode", app.address().port, app.settings.env);
+    });
   });
-    mongoose.connect('mongodb://localhost/exampleDB');
-    var server = new Server(models, 6379, 'localhost', sio.sockets);
-
-  app.listen(8080);
-  console.log("Started test server at port: %d in %s mode", app.address().port, app.settings.env);
 });
+
+var mongooseStorage = new Gnd.MongooseStorage(models);
+var pubClient = redis.createClient(6379, "127.0.0.1"),
+    subClient = redis.createClient(6379, "127.0.0.1");
+
+var syncHub = new Gnd.Sync.SyncHub(pubClient, subClient, sio.sockets);
+var gndServer = new Gnd.Server(mongooseStorage, syncHub);
+var socketServer = new Gnd.SocketBackend(sio.sockets, gndServer);
+
