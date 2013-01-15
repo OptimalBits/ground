de7d3b279c3b2ded3773f817c452abfffdc2d1c9
Aron Kornhall <git@kornhall.se>
2013-01-15 15:37:26 +0100

    Improved chat example

--- examples/example_server.js
+++ examples/example_server.js
@@ -21,33 +21,39 @@ var Message = new Schema({
   text : {type: String}
 });
 
-var Chat = new Schema({
-  messages :  [{ type: Schema.ObjectId, ref: 'Message' }]
+var Room = new Schema({
+  name : {type: String},
+  url: {type: String},
+  messages :  [{type: Schema.ObjectId, ref: 'Message'}]
 });
 
-Chat.statics.add = function(id, setName, itemIds, cb){
+var Chat = new Schema({
+  rooms :  [{type: Schema.ObjectId, ref: 'Room'}]
+});
+
+Chat.statics.add = Room.statics.add = function(id, setName, itemIds, cb){
   var update = {$addToSet: {}};
   update.$addToSet[setName] = {$each:itemIds};
   this.update({_id:id}, update, cb);
 };
 
 var models = {
-  Message: mongoose.model('Message', Message),
   messages: mongoose.model('Message', Message),
+  rooms: mongoose.model('Room', Room),
   Chat: mongoose.model('Chat', Chat)
 };
 
 // Setup mongodb
 mongoose.connect('mongodb://localhost/exDB', function(){
-  mongoose.connection.db.executeDbCommand( {dropDatabase:1}, function(err, result) {
-    console.log(result);
-    mongoose.disconnect(function(){
-      mongoose.connect('mongodb://localhost/exDB');
+  // mongoose.connection.db.executeDbCommand( {dropDatabase:1}, function(err, result) {
+  //   console.log(result);
+  //   mongoose.disconnect(function(){
+  //     mongoose.connect('mongodb://localhost/exDB');
   
       app.listen(8080);
       console.log("Started test server at port: %d in %s mode", app.address().port, app.settings.env);
-    });
-  });
+  //   });
+  // });
 });
 
 var mongooseStorage = new Gnd.MongooseStorage(models);
