888f3ac0b83eeb50153ccaadf499a419358a24b2
Manuel Astudillo <manuel@optimalbits.com>
2013-02-26 21:00:58 +0100

    Updated build script to also compile the server.

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
+echo "Compiling typescript..."
 tsc --sourcemap --out dist/gnd.js gnd.ts
-uglifyjs dist/gnd.js -c -m > dist/gnd.min.js
+uglifyjs dist/gnd.js -c warnings=false -m > dist/gnd.min.js
+tsc --out dist/gnd-server.js gnd-server.ts
