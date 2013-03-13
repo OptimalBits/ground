88b3e5854bcdb61e378413b3eb11c406a0364c0c
Manuel Astudillo <manuel@optimalbits.com>
2013-03-13 21:54:29 +0100

    Updated View unit test.

--- test/test_view.js
+++ test/test_view.js
@@ -5,15 +5,15 @@ describe('View', function(){
   it('Create view based on html', function(){
     var $views = Gnd.$('#views');
     
-    var view = new Gnd.View({
-      html: '<div><p>hello world</p><div>',
-      root: $views[0]
+    var view = new Gnd.View('#views', {
+      html: '<div><p>hello world</p></div>'
     });
     
     view.render();
     
-    expect($views[0].childNodes.length).to.be(3);
-    
+    expect($views[0].childNodes.length).to.be(1);
+    expect($views[0].childNodes[0].childNodes.length).to.be(1);
+    expect($views[0].childNodes[0].childNodes[0].innerHTML).to.be('hello world');
   });
   
 });
