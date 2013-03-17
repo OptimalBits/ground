477c6f63fcfbaedb440b29557c553fbf5bccf1ab
Manuel Astudillo <manuel@optimalbits.com>
2013-03-17 22:19:29 +0100

    Updated gnd module to expose file paths for its dist files.

--- index.js
+++ index.js
@@ -2,3 +2,12 @@
 // does not allow us to generate a single file CommonJS module with imports.
 _ = require('underscore');
 module.exports = require('./dist/gnd-server');
+
+var path = require('path');
+module.exports.lib = path.join(__dirname, 'dist/gnd.min.js');
+module.exports.map = path.join(__dirname, 'dist/gnd.js.map');
+module.exports.debug = path.join(__dirname, 'dist/gnd.js');
+
+module.exports.third = {
+  curl: path.join(__dirname, 'third/curl.js')
+}
