cb72d0c211eb78120a34f33bb1925042259c1394
Manuel Astudillo <manuel@optimalbits.com>
2013-05-24 14:48:54 +0200

    fixed views unit test

--- test/test_view.js
+++ test/test_view.js
@@ -6,11 +6,11 @@ describe('View', function(){
   it('Create view based on html', function(){
     var $views = Gnd.$('#views');
     
-    var view = new Gnd.View('#views', null, {
+    var view = new Gnd.View({
       html: '<div><p>hello world</p></div>'
     });
     
-    view.render();
+    view.parent('#views').render();
     
     expect($views[0].childNodes.length).to.be(1);
     expect($views[0].childNodes[0].childNodes.length).to.be(1);
