f688d2178ed092c4d287f727ad7054162222b8a7
Manuel Astudillo <manuel@optimalbits.com>
2013-06-19 16:39:25 +0200

    added base and debugBase with paths to ground compatible with AMD loaders.

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ var path = require('path');
 module.exports.lib = path.join(__dirname, 'dist/gnd.min.js');
 module.exports.map = path.join(__dirname, 'dist/gnd.js.map');
 module.exports.debug = path.join(__dirname, 'dist/gnd.js');
+module.exports.base = path.join(__dirname, 'dist/gnd.min');
+module.exports.debugBase = path.join(__dirname, 'dist/gnd');
 
 module.exports.third = {
   curl: path.join(__dirname, 'third/curl.js'),
