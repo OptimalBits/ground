d393b21d2097e8be76827925209cea0f67df431a
Manuel Astudillo <manuel@optimalbits.com>
2013-07-12 21:07:36 +0200

    refactored schema types

--- lib/models/schema.ts
+++ lib/models/schema.ts
@@ -3,8 +3,8 @@
   MIT Licensed.
 */
 /**
-  Model Schema.
-  Compatible schema with MoongoseJs, to be used both in server and client side.
+  Schema.
+  Inspired by Moongosejs, to be used both in server and client side.
 */
 /**
 var AnimalSchema = new Schema({
@@ -25,89 +25,11 @@ Base model schema:
 }
 */
 
+/// <reference path="schemaType" />
 /// <reference path="../promise" />
 
 module Gnd
-{  
-  interface ISchemaType
-  {
-    new (definition): SchemaType;
-  }
-   
-  export class SchemaType
-  { 
-    public definition;
-    
-    constructor(definition)
-    {
-      this.definition = definition;
-    }
-    
-    validate(val: any): Promise
-    {
-      return new Promise(true);
-    }
-    
-    toObject(obj)
-    {
-      return obj;
-    }
-    
-    default()
-    {
-      return this.definition.default;
-    }
-  }
-  
-  class StringType extends SchemaType
-  {
-  }
-  
-  class NumberType extends SchemaType
-  {
-  }
-  
-  class BooleanType extends SchemaType
-  {
-  }
-  
-  class DateType extends SchemaType
-  {
-  }
-  
-  class ObjectIdType extends SchemaType
-  {
-  }
-  
-  class ArrayType extends SchemaType
-  {
-    private schema: Schema;
-    
-    constructor(def)
-    {
-      super(def);
-      var type = (def.type || def)[0];
-      this.schema = Schema.compileType(type, def);
-    }
-    
-    toObject(arr)
-    {
-      return _.map(arr, (item) => this.schema.toObject(item));
-    }
-  }
-  
-  // AbstractType is a special type that reads __schema from the
-  // obj. This is in order to allow abstract schemas whose implementations
-  // are not known until runtime.
-  class AbstractType extends SchemaType
-  {
-    toObject(obj)
-    {
-      var schema = obj.__schema || obj.constructor.__schema;
-      return schema ? schema.toObject(obj): undefined;
-    }
-  }
-  
+{ 
   /*
   export interface SchemaDefinition{
     [index: string]: SchemaType;
@@ -126,28 +48,31 @@ module Gnd
       this.compiledSchema = this.compile(schema || {});
     }
     
-    validate(obj: any, property?: string, value?: any): Promise // Promise<bool>
+    validate(obj: any, property?: string, value?: any): Promise<bool>
     {
       if(property){
         return this.compiledSchema[property].validate(value);
       }else{
+        return new Promise<bool>(true);
+        /*
         return Promise.map(_.keys(this.schema), (property) =>
           this.validate(obj, property, obj[property]));
+          */
       }
     }
-
+  
     /**
       toObject - Takes the corresponding properties from the given obj
       according to the schema and returns them in a plain object compatible
       with JSON.stringify.
       
-      extraSchemas is an object pairing properties with Schemas. This is useful
+      extra is an object pairing properties with Schemas. This is useful
       to override some properties Schemas in run time.
     */
     toObject(obj: any, extra?: {[index: string]: Schema;}): any
     {
       var result = {};
-      
+
       _.each(this.compiledSchema, (type: SchemaType, property?) => {
         var src = obj[property];
         if(!_.isUndefined(src)){
@@ -162,6 +87,17 @@ module Gnd
         }
       });
       return result;
+    }
+
+    //get(obj: any, args?: {}, opts?);
+    get(obj: any, key?: string, args?: {}, opts?: {})
+    {
+      if(_.isString(key)){
+        var schema = this.schema[key];
+        return schema && schema.get && schema.get(obj, args, opts);
+      }else{
+        return schema;
+      }
     }
 
     default(){
@@ -190,6 +126,7 @@ module Gnd
           if(compiledType){
             compiledSchema[property] = compiledType;
           }else{
+            console.log(definition)
             throw Error("Invalid type definition:"+definition);
           }
         }
@@ -210,7 +147,7 @@ module Gnd
     {
       var types = Schema.types;
       
-      if(type instanceof Schema){
+      if(type instanceof Schema || type instanceof SchemaType){
         return type;
       }
 
@@ -219,26 +156,38 @@ module Gnd
       }
 
       for(var i=0; i<types.length; i++){
-        if(types[i].type == type && types[i].Class){
-          return new types[i].Class(definition);
+        if(types[i].type == type){
+          return new types[i](definition);
         }
       }
+    }
+    
+    public map(iter: (key, value) => any)
+    {
+      var result = {};
+      _.each(this.compiledSchema, (value, key?) => {
+        var def = iter(key, value);
+        if(def) result[key] = def;
+      });
+      return result;
     }
 
     public static ObjectId = 'ObjectId';
     public static Abstract = 'Abstract';
-    public static Buffer;
+
+    // needed?
     public static Mixed;
+    public static Buffer;
     
     // {type: any; Class: ISchemaType;}
-    private static types: any[] = [
-      {type: String, Class: StringType},
-      {type: Number, Class: NumberType},
-      {type: Boolean, Class: BooleanType},
-      {type: Date, Class: DateType},
-      {type: Object, Class: SchemaType},
-      {type: Schema.ObjectId, Class: ObjectIdType},
-      {type: Schema.Abstract, Class: AbstractType}
+    private static types: ISchemaType[] = [
+      StringType,
+      NumberType,
+      BooleanType,
+      DateType,
+      SchemaType,
+      ObjectIdType,
+      AbstractType
     ];
   }
   
---
+++ lib/models/schemaType.ts
@@ -0,0 +1,113 @@
+/**
+  Ground Web Framework (c) 2012 Optimal Bits Sweden AB
+  MIT Licensed.
+*/
+/**
+  SchemaTypes
+  Inspired by Moongosejs, to be used both in server and client side.
+*/
+
+/// <reference path="../promise" />
+/// <reference path="schema" />
+
+module Gnd
+{   
+  /*
+  class Schema{
+    public static ObjectId = 'ObjectId';
+    public static Abstract = 'Abstract';
+
+    // needed?
+    public static Mixed;
+    public static Buffer;
+  }
+  */
+  
+  export interface ISchemaType
+  {
+    type;
+    new (definition): SchemaType;
+  }
+   
+  export class SchemaType
+  { 
+    public static type: any = Object;
+    public definition;
+    
+    constructor(definition)
+    {
+      this.definition = definition;
+    }
+    
+    validate(val: any): Promise<bool>
+    {
+      return new Promise(true);
+    }
+    
+    toObject(obj)
+    {
+      return obj;
+    }
+
+    default()
+    {
+      return this.definition.default;
+    }
+  }
+  
+  export class StringType extends SchemaType
+  {
+    public static type = String;
+  }
+  
+  export class NumberType extends SchemaType
+  {
+    public static type = Number;
+  }
+  
+  export class BooleanType extends SchemaType
+  {
+    public static type = Boolean;
+  }
+  
+  export class DateType extends SchemaType
+  {
+    public static type = Date;
+  }
+  
+  export class ObjectIdType extends SchemaType
+  {
+    public static type = 'ObjectId';
+  }
+  
+  export class ArrayType extends SchemaType
+  {
+    public static type = Array;
+    private schema: Schema;
+    
+    constructor(def)
+    {
+      super(def);
+      var type = (def.type || def)[0];
+      this.schema = Schema.compileType(type, def);
+    }
+    
+    toObject(arr)
+    {
+      return _.map(arr, (item) => this.schema.toObject(item));
+    }
+  }
+  
+  // AbstractType is a special type that reads __schema from the
+  // obj. This is in order to allow abstract schemas whose implementations
+  // are not known until runtime.
+  export class AbstractType extends SchemaType
+  {
+    public static type = 'Abstract';
+    toObject(obj)
+    {
+      var schema = obj.__schema || obj.constructor.__schema;
+      return schema ? schema.toObject(obj): undefined;
+    }
+  }
+}
